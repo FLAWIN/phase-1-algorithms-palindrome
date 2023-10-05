a384f305bfe23d77921d1ba93c2a630100b9893b
Aloe Flawin <aloeflawingulenywa98@gmail.com>
2023-10-05 10:01:47 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {                  // or from 4 - 8 : return word === reversedWord
+    return false;
+  }
 }
 
 /* 
@@ -9,17 +16,27 @@ function isPalindrome(word) {
 /*
   Add written explanation of your solution here
 */
+function reverseString(word) {
+  // create an array from the input string
+  const wordArray = word.split("");
+  console.log(wordArray)
+  // reverse the array
+  const reversedWordArray = wordArray.reverse(); // or this method body to be
+  console.log(reversedWordArray)                 // return word.split('').reverse().join('')
+  // create a string from the reversed array
+  const reversedWord = reversedWordArray.join("");
+  console.log(reversedWord)
+  // return the reversed string
+  return reversedWord;
+}
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
-
   console.log("");
-
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
-
 module.exports = isPalindrome;
